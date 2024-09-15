09890d35dde68a1992db111f25da3b6fb819593a
Tywina Tracy <140459991+tyw03@users.noreply.github.com>
2024-09-15 01:50:05 +0000

    commit project_one

---
+++ project_one/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int* bubblesort(int arr[], int num_of_elements) {
+    for(int i=1; i<=num_of_elements-1; i++) {
+        for(int j=num_of_elements-1; j>=i; j--) {
+            if(arr[j] < arr[j-1]) {
+                int prev_j_value = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = prev_j_value;
+            }
+        }
+    }
+    return arr;
+}
+
+void print_array(int arr[], int num_of_elements) {
+    for (int i=0; i<num_of_elements; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    int num_of_elements;
+    cin >> num_of_elements;
+
+    int array[num_of_elements];
+    for (int index=0; index<num_of_elements; index++) {
+        cin >> array[index];
+    }
+
+    int* sorted_array = bubblesort(array, num_of_elements);
+    print_array(sorted_array, num_of_elements);
+
+    return 0;
+}
